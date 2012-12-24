e7fc65cb2457ca084d27858f04ccfa640a4f5fb7
Ryan Scott Lewis <ryan@rynet.us>
2012-12-23 23:55:50 -0500

    Break out parsing and evaluation logic

--- examples/sample.lua
+++ examples/sample.lua
@@ -3,23 +3,17 @@ package.cpath = package.cpath .. ";../pkg/?.so"
 
 require "mruby"
 
-mruby.run(" puts 'Hello, World!'" )
+mruby.run(" puts 'Hello, World!' " )
+-- print(
+ 
+-- )
 
 
--- mruby.run([[
--- 
--- def tak(x, y, z)
--- 
---     if y >= x
---         z
---     else
---         tak(tak(x - 1, y, z),tak(y - 1, z, x),tak(z - 1, x, y))
---     end
--- end
--- 
--- print tak(20, 12, 7)
--- 
--- ]])
+-- print(
+--   mruby.run([[
+--     sdlfjbnlsjdfbn
+--   ]])
+-- )
 
 -- mrbState = mrbOpen()
 -- print( mrbState ) -- Print out the MRuby state
--- lib/lua-mruby.c
+++ lib/lua-mruby.c
@@ -7,20 +7,61 @@
 #include "mruby/compile.h"
 
 
-
+# NOTE: lib/mruby/tools/mirb.c has almost all this functionality created by someone way better
+#       at this than I am. Pull code from there into here. =)
 
 static int lua_mruby_run(lua_State *L) {
-  mrb_state *mrb = mrb_open();
+  mrb_state *mrb_interpreter = mrb_open();
   const char *mrb_code = lua_tostring(L, 1);
-  struct mrb_parser_state* p = mrb_parse_string(mrb, mrb_code, NULL);
-  int n = mrb_generate_code(mrb, p);
+  struct mrb_parser_state *mrb_parser = mrb_parser_new(mrb_interpreter);
+  mrbc_context *mrb_context;
+  mrb_value mrb_return_value;
+    
+  mrb_context = mrbc_context_new(mrb_interpreter);
+  mrb_context->capture_errors = 1;
   
-  mrb_run( mrb, mrb_proc_new(mrb, mrb->irep[n]), mrb_nil_value() );
+  // Parse the code - TODO: Convert into function
+  mrb_parser->s = mrb_code;
+  mrb_parser->send = mrb_code + strlen(mrb_code);
+  mrb_parser->capture_errors = 1;
+  mrb_parser->lineno = 1;
   
-  int lua_return = n > 0 ? 0 : 1;
-  lua_pushnumber(L, lua_return);
+  mrb_parser_parse(mrb_parser, mrb_context);
   
-  mrb_close(mrb);
+  if (0 < mrb_parser->nerr) {
+    printf("%d\n", mrb_parser->nerr);
+    // Check for parser error - TODO: Convert into function
+    char *mrb_error_message;
+    size_t sz;
+    sz = snprintf(NULL, 0, "line %d: %s\n", mrb_parser->error_buffer[0].lineno, mrb_parser->error_buffer[0].message);
+    mrb_error_message = malloc(sz + 1); /* make sure you check for != NULL in real code */
+    snprintf(mrb_error_message, sz+1, "line %d: %s\n", mrb_parser->error_buffer[0].lineno, mrb_parser->error_buffer[0].message);
+    
+    lua_pushstring(L, mrb_error_message);
+  } else { 
+    // TODO: Convert into function:
+    // Generate bytecode
+    int mrb_byte_code = mrb_generate_code(mrb_interpreter, mrb_parser);
+    // Generate Proc from bytecode for evaulation
+    struct RProc *mrb_proc = mrb_proc_new( mrb_interpreter, mrb_interpreter->irep[mrb_byte_code] );
+    // Evaluate the bytecode
+    mrb_return_value = mrb_run(mrb_interpreter, mrb_proc, mrb_nil_value());
+    // Check for exception
+    if (mrb_interpreter->exc) {
+      mrb_p(mrb_interpreter, mrb_obj_value(mrb_interpreter->exc));
+      mrb_interpreter->exc = 0;
+    } else {
+      int *lua_return = mrb_return_value.value.p;
+      printf("%d\n", *lua_return);
+      int lua_return_number = *lua_return > 0 ? 0 : 1;
+      lua_pushnumber(L, lua_return_number);
+    }
+    
+  }
+  
+  mrb_parser_free(mrb_parser);
+  mrbc_context_free(mrb_interpreter, mrb_context);
+  mrb_close(mrb_interpreter);
   
   return 1;
 }
@@ -34,27 +75,63 @@ static const struct luaL_reg mrubylib[] = {
 
 
 
-static int lua_mrb_open(lua_State *L) {
-  mrb_state *mrb = mrb_open();
-  
-  lua_pushlightuserdata(L, &mrb);
-  
-  return 1;
-}
 
-static int lua_mrb_close(lua_State *L) {
-  mrb_state *mrb = lua_topointer(L, 1); // Convert the first argument to a C pointer
-  
-  mrb_close(mrb);
-  
-  return 1;
-}
+////////////////////////////////////////
+//// EXAMPLE OF PUSHING STRUCT TO LUA //
+////////////////////////////////////////
+// 
+// typedef struct MyStruct_s { 
+//   int value1; 
+//   const char * str; 
+// } MyStruct_t; 
+// 
+// void MyStruct_toTable (MyStruct_t *obj, lua_State *L) 
+// { 
+//   lua_newtable(L); 
+//   lua_pushnumber(L, obj->value1); 
+//   lua_setfield(L,-2,"value1"); 
+//   lua_pushstring(L,obj->str); 
+//   lua_setfield(L,-2,"str"); 
+// }
 
-static const struct luaL_reg mrblib[] = {
-  { "mrbOpen" , lua_mrb_open  },
-  { "mrbClose", lua_mrb_close },
-  { NULL, NULL }
-};
+
+
+
+
+
+
+
+// static int lua_mrb_nil_value(lua_State *L) {
+//   mrb_value value = mrb_nil_value();
+//   
+//   lua_pushlightuserdata(L, &value);
+//   
+//   return 1;
+// }
+// 
+// static int lua_mrb_open(lua_State *L) {
+//   mrb_state *mrb = mrb_open();
+//   
+//   
+//   lua_pushlightuserdata(L, mrb);
+//   
+//   return 1;
+// }
+// 
+// static int lua_mrb_close(lua_State *L) {
+//   mrb_state *mrb = lua_topointer(L, 1); // Convert the first argument to a C pointer
+//   
+//   mrb_close(mrb);
+//   
+//   return 1;
+// }
+// 
+// static const struct luaL_reg mrblib[] = {
+//   { "mrbNilValue" , lua_mrb_nil_value },
+//   { "mrbOpen"     , lua_mrb_open      },
+//   { "mrbClose"    , lua_mrb_close     },
+//   { NULL, NULL }
+// };
 
 
 
@@ -62,8 +139,8 @@ static const struct luaL_reg mrblib[] = {
 
 int luaopen_mruby(lua_State *L) {
   // luaL_register(L, NULL, mrblib);
-  lua_register(L, "mrbOpen", lua_mrb_open);
-  lua_register(L, "mrbClose", lua_mrb_close);
+  // lua_register(L, "mrbOpen", lua_mrb_open);
+  // lua_register(L, "mrbClose", lua_mrb_close);
   
   luaL_register(L, "mruby", mrubylib);
   return 1;
